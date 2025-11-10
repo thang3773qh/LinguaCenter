6525ff3e9eca214a217d6587931e57dfcf1f77fb
Pham_Thang <thang3773qh@gmail.com>
2025-11-10 09:53:05 +0700

    this pushhhhhhhhh

---
+++ Trung tâm ngoại ngữ/Models/TbCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinguaCenter.Models;
+
+public partial class TbCategory
+{
+    public int CategoryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Alias { get; set; }
+
+    public string? Image { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public virtual ICollection<TbCourse> TbCourses { get; set; } = new List<TbCourse>();
+}
---
+++ Trung tâm ngoại ngữ/Models/TbContact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinguaCenter.Models;
+
+public partial class TbContact
+{
+    public int ContactId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Message { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+}

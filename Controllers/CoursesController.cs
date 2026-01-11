b5abd29133e940ec64d1d40abf4ad03568dbbf89
thang3773qh <73017623+thang3773qh@users.noreply.github.com>
2026-01-11 08:59:24 +0700

    Add files via upload

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,38 @@
+﻿using LinguaCenter.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinguaCenter.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly LinguaCenterContext _context;
+        public CoursesController(LinguaCenterContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/course/{alias}-{id}.html")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TbCourses == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.TbCourses.Include(i => i.Trainer)
+                .FirstOrDefaultAsync(m => m.CourseId == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.OrderCount = await _context.TbOrders.CountAsync(o => o.CourseId == id);
+
+            return View(course);
+        }
+    }
+}
